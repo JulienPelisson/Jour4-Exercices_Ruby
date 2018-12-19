2e435c43d9a22eae6bcca94be29ef24b4b30b806
JulienTHP <45801226+JulienTHP@users.noreply.github.com>
2018-12-19 22:11:07 +0100

    Add files via upload

---
+++ exo_02.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, monde !"
+puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+end
+
+# La différence entre puts et print est la suivante : puts ajoutera
+# une nouvelle ligne de texte
+# Print au contraire ajoutera tout à la suite sur la même ligne. 
+
+
---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, monde !"
+#puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# le caractère "#" sert à mettre un commentaire dans le code.
